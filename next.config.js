363c1760177f9858a67513193836a5220c493f0a
ilakkiyanj <ilakkiyanj01@gmail.com>
2024-09-03 15:32:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+// next.config.js
 module.exports = {
   output: 'export',
   trailingSlash: true,
+  basePath: '', // Use this if deploying to a subpath
+  assetPrefix: '', // Use this if deploying to a subpath
 }
